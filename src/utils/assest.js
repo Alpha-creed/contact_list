136c5cfc16dc5f2da2f2eccbc26ddaeb6ebc3093
Alphacreed <wynoral@gmail.com>
2023-08-15 03:26:24 +0000

    initail commit

--- src/utils/assest.js
+++
@@ -1,5 +0,0 @@
-export const phoneIcon = <i class="fa-solid fa-phone"></i>
-export const newIcon = <i class="fa-solid fa-circle-plus"></i>
-export const viewIcon = <i class="fa-solid fa-eye"></i>
-export const editIcon = <i class="fa-solid fa-pen"></i>
-export const deleteIcon = <i class="fa-solid fa-trash"></i>
